4b3855cd731f17aa48ce37bbb978ea2c4b385878
Eric de Quartel <edequartel@protonmail.com>
2024-01-08 06:47:59 +0100

    Initial Commit

---
+++ Ravens/RavensApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavensApp.swift
+//  Ravens
+//
+//  Created by Eric de Quartel on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RavensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
