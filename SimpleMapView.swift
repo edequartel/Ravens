62f1ff7b240f35723513d34a6b97f34f52217d3a
Eric de Quartel <edequartel@protonmail.com>
2024-03-18 13:24:44 +0100

    update after mistake of deleting files

--- SimpleMapView.swift
+++ SimpleMapView.swift
@@ -2,17 +2,9 @@ import SwiftUI
 import MapKit
 
 struct SimpleMapView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
-    @State private var tracking: MapUserTrackingMode = .follow
-
+    
     var body: some View {
-        Map(coordinateRegion: $region,
-            interactionModes: .all,
-            showsUserLocation: true,
-            userTrackingMode: $tracking)
+        Text("Hello, World!")
     }
 }
 
