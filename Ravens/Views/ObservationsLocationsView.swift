1df4b7d04b57c6aaacac4a779a5689401d071772
Eric de Quartel <edequartel@protonmail.com>
2024-04-08 06:50:57 +0200

    update

--- Ravens/Views/ObservationsLocationsView.swift
+++ Ravens/Views/ObservationsLocationsView.swift
@@ -21,20 +21,12 @@ struct ObservationsLocationView: View {
     
     var locationId: Int
     var locationStr: String
-
+    
     @Binding var isShowing: Bool
     
     var body: some View {
         VStack {
-                HStack {
-                    Text("Location")
-//                        .font(.title)
-
-//                    Text(locationStr)
-                }
-            .padding(16)
-            .bold()
-            
+            Text("Locations")
             List {
                 if let results =  viewModel.observations?.results {
                     ForEach(results.sorted(by: { ($1.rarity, $0.species_detail.name,  $1.date, $0.time ?? "00:00") < ($0.rarity, $1.species_detail.name, $0.date, $1.time ?? "00:00") }), id: \.id) {
