22a4d72cfdcf65954b1d459d8bc5199dee8f25fa
Eric de Quartel <edequartel@protonmail.com>
2024-04-08 09:21:33 +0200

    update

--- Ravens/Models/ObservationsLocationViewModel.swift
+++ Ravens/Models/ObservationsLocationViewModel.swift
@@ -100,11 +100,13 @@ class ObservationsLocationViewModel: ObservableObject {
         AF.request(url, headers: headers).responseString { response in
             switch response.result {
             case .success(let stringResponse):
-                // Now you can convert the stringResponse to Data and decode it
-                if let data = stringResponse.data(using: .utf8) {
+                // Convert the stringResponse to Data using the original encoding and decode it
+                if let data = stringResponse.data(using: .isoLatin1),
+                   let utf8String = String(data: data, encoding: .isoLatin1),
+                   let utf8Data = utf8String.data(using: .utf8) {
                     do {
                         let decoder = JSONDecoder()
-                        let observationsSpecies = try decoder.decode(Observations.self, from: data)
+                        let observationsSpecies = try decoder.decode(Observations.self, from: utf8Data)
 
                         DispatchQueue.main.async {
                             self.observations = Observations(results: observationsSpecies.results)
@@ -121,6 +123,30 @@ class ObservationsLocationViewModel: ObservableObject {
                 self.log.error("Error ObservationsLocationViewModel: \(error)")
             }
         }
+//        AF.request(url, headers: headers).responseString { response in
+//            switch response.result {
+//            case .success(let stringResponse):
+//                // Now you can convert the stringResponse to Data and decode it
+//                if let data = stringResponse.data(using: .utf8) {
+//                    do {
+//                        let decoder = JSONDecoder()
+//                        let observationsSpecies = try decoder.decode(Observations.self, from: data)
+//
+//                        DispatchQueue.main.async {
+//                            self.observations = Observations(results: observationsSpecies.results)
+//                            self.getLocations()
+//                            self.getSpan()
+//                            completion()
+//                        }
+//                    } catch {
+//                        self.log.error("Error ObservationsLocationViewModel decoding JSON: \(error)")
+//                        self.log.error("\(url)")
+//                    }
+//                }
+//            case .failure(let error):
+//                self.log.error("Error ObservationsLocationViewModel: \(error)")
+//            }
+//        }
     }
 }
 
