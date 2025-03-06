763bc01151be06e6345202dc115c59358690c631
Eric de Quartel <edequartel@protonmail.com>
2025-03-06 18:36:01 +0100

    update

--- ExtensionsAndFunctions.swift
+++ ExtensionsAndFunctions.swift
@@ -11,8 +11,8 @@ import AudioToolbox
 import SFSafeSymbols
 import MapKit
 
-let showView = false
-//let showView = true
+//let showView = false
+let showView = true
 
 // werkhoven
 // let longitude = 5.243376
